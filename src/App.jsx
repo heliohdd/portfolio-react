972346b65e92c973656082fdaacc4e1e599f473e
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-02 14:28:14 -0300

    Create Website Header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 };
 
 export default App;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I`m Alex Bennet,</span> frontend developer based in USA.
+      </h1>
+      <p>
+        I`m a frontend developer from California, USA with 10 years of
+        experience in multiple companies like Microsoft, Tesla and Apple.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect With Me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
