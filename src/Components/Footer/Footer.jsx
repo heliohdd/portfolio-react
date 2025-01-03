c5e2d59d16e0d0af9994bed5b35d752511d8eb69
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-03 14:36:06 -0300

    Create Footer for the website

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Footer.css";
+import footer_logo from "../../assets/footer_logo.svg";
+import user_icon from "../../assets/user_icon.svg";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={footer_logo} alt="" />
+          <p>
+            I am a fronted developer from Brazil with 10 years of experience in
+            companies like Microsoft, Tesla and Apple.
+          </p>
+        </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Enter your email" />
+          </div>
+          <div className="footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          © 2023 Hélio Dourado. All rigths reserved.
+        </p>
+      </div>
+      <div className="foot-bottom-right">
+        <p>Term of Services</p>
+        <p>Privacy Policy</p>
+        <p>Connect with me</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
