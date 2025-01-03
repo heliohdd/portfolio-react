bc72e851d6a1c0672deadb27a70c41dad410d5a7
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-03 15:19:55 -0300

    Add Smooth Scroll on Website

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.jpg";
 // import profile_img from "../../assets/profile_img.svg";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profile_img} alt="" height={500} />
       <h1>
         <span>I`m Alex Bennet,</span> frontend developer based in USA.
@@ -15,7 +16,11 @@ const Hero = () => {
         experience in multiple companies like Microsoft, Tesla and Apple.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect With Me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import "./Navbar.css";
 import logo from "../../assets/logo.svg";
 import underline from "../../assets/nav_underline.svg";
@@ -10,27 +11,41 @@ const Navbar = () => {
       <img src={logo} alt="" />
       <ul className="nav-menu">
         <li>
-          <p onClick={() => setMenu("home")}>Home</p>
+          <AnchorLink className="anchor-link" href="#home">
+            <p onClick={() => setMenu("home")}>Home</p>
+          </AnchorLink>
           {menu === "home" ? <img src={underline} alt="" /> : <></>}
         </li>
         <li>
-          <p onClick={() => setMenu("about")}>About Me</p>
+          <AnchorLink className="anchor-link" offset={50} href="#about">
+            <p onClick={() => setMenu("about")}>About Me</p>
+          </AnchorLink>
           {menu === "about" ? <img src={underline} alt="" /> : <></>}
         </li>
         <li>
-          <p onClick={() => setMenu("services")}>Services</p>
+          <AnchorLink className="anchor-link" offset={50} href="#services">
+            <p onClick={() => setMenu("services")}>Services</p>
+          </AnchorLink>
           {menu === "services" ? <img src={underline} alt="" /> : <></>}
         </li>
         <li>
-          <p onClick={() => setMenu("work")}>Portfolio</p>
-          {menu === "work" ? <img src={underline} alt="" /> : <></>}
+          <AnchorLink className="anchor-link" offset={50} href="#mywork">
+            <p onClick={() => setMenu("mywork")}>Portfolio</p>
+          </AnchorLink>
+          {menu === "mywork" ? <img src={underline} alt="" /> : <></>}
         </li>
         <li>
-          <p onClick={() => setMenu("contact")}>Contact</p>
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            <p onClick={() => setMenu("contact")}>Contact</p>
+          </AnchorLink>
           {menu === "contact" ? <img src={underline} alt="" /> : <></>}
         </li>
       </ul>
-      <div className="nav-connect">Connect With Me</div>
+      <div className="nav-connect">
+        <AnchorLink className="anchor-link" offset={50} href="#contact">
+          Connect With Me
+        </AnchorLink>
+      </div>
     </div>
   );
 };
